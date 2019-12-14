1e5a738f18582c2e5ec61aa73900adec706da268
Akshay <aks28id@gmail.com>
2019-12-14 14:07:45 +0530

    dark theme

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
 import React, { useState, Component } from "react";
 import { Router, Link } from "@reach/router";
-import { Helmet } from "react-helmet";
-
 import HomePage from "./pages/HomePage";
 import RecordPage from "./pages/RecordPage";
+
+require("@elastic/eui/dist/eui_theme_dark.css");
 
 const setTheme = theme => {
   localStorage.setItem("theme", theme);
   window.location.reload();
 };
 
-var links = '';
-
 export default class App extends Component {
   componentWillMount() {
-    if (localStorage.getItem("theme") === "dark") {
-        links = [
-        {
-          rel: "stylesheet",
-          href: `/static/css/eui_theme_dark.css`
-        }
-      ];
-    } else {
-        links = [
-        {
-          rel: "stylesheet",
-          href: `/static/css/eui_theme_light.css`
-        }
-      ];
-    }
+    // if (localStorage.getItem("theme") === "dark") {
+    //   require("@elastic/eui/dist/eui_theme_dark.css");
+    //   let stylesheet = document.styleSheets[0];
+    //   stylesheet.href = "@elastic/eui/dist/eui_theme_dark.css";
+    // } else {
+    //   require("@elastic/eui/dist/eui_theme_light.css");
+    //   let stylesheet = document.styleSheets[0];
+    //   stylesheet.href = "@elastic/eui/dist/eui_theme_light.css";
+    // }
   }
 
   render() {
     return (
       <Router>
-        <Helmet path="*" links={links} />
         <HomePage path="/" setTheme={setTheme} />
         <RecordPage path="recording" />
       </Router>
